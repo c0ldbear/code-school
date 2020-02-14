3f355821c06ebee46b5c6f9ddf5fd21c8c69efba
teddy <teddyjh@protonmail.com>
2020-02-14 11:32:02 +0100

    add outputs to terminal to see the variables
    Since debug mode does not work in VS Code Insider for MacOS, we need
    to make our own "debug" which will be to display our variables to
    the terminal - for now.

--- ep01/ep01.cpp
+++ ep01/ep01.cpp
@@ -6,6 +6,13 @@ int main() {
     char A = 'A';
     float money = 104.52;
 
+    // Display the output of the variables
+    std::cout << "int numberOfMonkeys = " << numberOfMonkeys << std::endl;
+    std::cout << "bool True = " << True << std::endl;
+    std::cout << "char A = " << A << std::endl;
+    std::cout << "float money = " << money << std::endl;
+    std::cout << std::endl << "Program done." << std::endl;
+
 
 }
 
