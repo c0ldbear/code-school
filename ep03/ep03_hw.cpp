826f186fd38d7b080d2e3305e8e1947d52e91a6b
teddy <teddyjh@protonmail.com>
2020-02-22 13:07:25 +0100

    add code for this episodes hw

--- ep03/ep03_hw.cpp
+++ ep03/ep03_hw.cpp
@@ -1,1 +1,25 @@
-int main() {}
+// can you use all types as conditions in a while loop?
+// a: no, not void
+
+// can you make an infinite while loop?
+// a: yes (while(true))
+
+// can you put scopes inside a while loop?
+// a: yes
+
+// can you put a while loop inside of a while loop?
+// a: yes
+
+// can you put a while loop inside a function? 
+// a: yes
+
+int main()
+{
+    int a = 0;
+    float b = 1.1230;
+    while (a < b)
+    {
+        a += 10;
+    }
+    return 0;
+}
