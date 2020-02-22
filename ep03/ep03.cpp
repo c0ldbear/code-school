8d763178e15e4d0079d8841fd3834791f2840fbf
teddy <teddyjh@protonmail.com>
2020-02-22 13:03:37 +0100

    add code from ep03
    playing around with while loops
    to simulate a fighting-scenario in a game.

--- ep03/ep03.cpp
+++ ep03/ep03.cpp
@@ -1,1 +1,27 @@
-int main() {}
+int do_damage(int health, int damage)
+// naming functions is hard because at the moment it can make sense.
+// not necessarily in the long run.
+{
+    return health - damage;
+}
+
+int main()
+{
+    //while(true) will have an infinite amount of fights
+    {
+        int player_health = 100;
+        int player_damage = 11;
+
+        int enemy_health = 100;
+        int enemy_damage = 50;
+        
+        // while (true) outside will just make the code run infinitly
+        // since it will be always satisfied meanwhile the inside-while loop
+        // will be done when one of the health is zero (or below).
+        while (enemy_health > 0 && player_health > 0)
+        {
+            enemy_health = do_damage(enemy_health, player_damage);
+            player_health = do_damage(player_health, enemy_damage);
+        }
+    }
+}
