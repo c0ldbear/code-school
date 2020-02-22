b65de79dfaf2925f46aa7e777910c4fb911f936c
teddy <teddyjh@protonmail.com>
2020-02-22 10:36:46 +0100

    add handling if one dies and test if hp < 0

--- ep02/ep02.cpp
+++ ep02/ep02.cpp
@@ -16,19 +16,20 @@ int main()
     int player_damage = 8;
     bool player_alive = true;
 
-    int enemy_health = 50;
+    int enemy_health = 7;
     int enemy_damage = 4;
     bool enemy_alive = true;
 
     player_health = attack(player_health, enemy_damage);
-    // player_health -= enemy_damage;
-    //if (player_health < 0)
-    //{
-        //player_health = 0;
-        //player_alive = false;
-    //}
-
+    if (player_health == 0)
+    {
+        player_alive = false;
+    }
     enemy_health  = attack(enemy_health, player_damage);
+    if (enemy_health == 0)
+    {
+        enemy_alive = false;
+    }
 
     return 0;
 
