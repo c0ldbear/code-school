92ca2fa5b437a98acd3a9acd4773f2dff47894a4
teddy <teddyjh@protonmail.com>
2020-02-22 14:44:46 +0100

    add pre-questions and assignment of this episode

---
+++ ep04/ep04_hw.cpp
@@ -0,0 +1,27 @@
+
+// preassignment questions
+
+// can you use all (data)types as member variables in a class?
+
+// can you use all types as return variables of class functions?
+
+// can a class have no member variables?
+
+// can a class have a member variable that is of another class?
+
+// can you use a class type as an argument in a function?
+
+// can you use a class type as a return value of a function?
+
+// assignment:
+// create a class that contains:
+// [] 1 char variable called 'letter'
+// [] 1 bool variable called 'is_uppercase'
+// [] 1 constructor that takes in 1 char variable called 'a_letter'
+// [] 1 function called 'bool make_uppercase();'
+//      if the letter is uppercase, it should do nothing and return false
+//      if the letter is lowercase, it should change it to uppercase and return true
+// [] 1 function called 'bool next_letter();'
+//      if the letter is between 'A' and 'Z', and 'a' and 'z', it should return a next letter
+//      otherwise it should not do anything. If success: return true, otherwise false
+// [] 1 function called 'void set_letter(char a_letter);'
