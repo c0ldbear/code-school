064b43cb5b7ad44b669c7497206daf2d255f99c1
teddy <teddyjh@protonmail.com>
2020-02-22 10:23:01 +0100

    add code for a potential RPG attack round
    we add code which mimics an attack round in a rpg game. We also have
    a new function to replace some part of the code.

--- ep02/ep02.cpp
+++ ep02/ep02.cpp
@@ -1,12 +1,37 @@
-int add(int number_a, int number_b)
+int attack(int health, int damage)
+// This is a pure function since it is modifying the data within it.
 {
-    return number_a + number_b;
+    health -= damage;
+    if (health < 0)
+    {
+        health = 0;
+    }
+
+    return 0;
 }
 
 int main()
 {
-    int sum = 5;
-    sum = add(sum, add(5,10));
+    int player_health = 100;
+    int player_damage = 8;
+    bool player_alive = true;
 
-    return sum;
+    int enemy_health = 50;
+    int enemy_damage = 4;
+    bool enemy_alive = true;
+
+    player_health -= enemy_damage;
+    if (player_health < 0)
+    {
+        player_health = 0;
+        player_alive = false;
+    }
+
+    enemy_health -= player_damage;
+    if (enemy_health < 0)
+    {
+        enemy_health = 0;
+        enemy_alive = false;
+    }
+
 }
