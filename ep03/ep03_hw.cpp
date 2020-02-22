3b98bed2f1e8adad6bd89ca772b79369d92f44c3
teddy <teddyjh@protonmail.com>
2020-02-22 13:43:19 +0100

    add solution to the homework and code{}school's solution too

--- ep03/ep03_hw.cpp
+++ ep03/ep03_hw.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 // can you use all types as conditions in a while loop?
 // a: no, not void
 
@@ -13,6 +15,84 @@
 // can you put a while loop inside a function? 
 // a: yes
 
+// assignment 01:
+// create a loop that goes through the entire alphabet from lowercase a to lowercase z
+void displayCharacter(char character)
+{
+    std::cout << character << std::endl;
+}
+
+void loopLowercaseAlphabet()
+{
+    char character = 'a';
+    while (character <= 'z')
+    {
+        displayCharacter(character);
+        character++;
+    }
+}
+
+// assignment 02:
+// create a loop that goes through the entire alphabet from lowercase a to lowercase z,
+// THEN goes through uppercase A to uppercase Z
+
+void loopLowercaseThenUppercaseAlphabetWeird()
+{
+    char character = 'a';
+    while (character != 'Z'+1) // not the most beautiful solution there is but...
+    {
+        if (character > 'z')
+        {
+            character -= ':'; 
+        }
+        displayCharacter(character);
+        character++;
+    }
+}
+
+void loopAlphabet()
+{
+    char character = 'a';
+
+    while (true)
+    {
+        displayCharacter(character);
+
+        // break condition for while-true
+        if (character == 'Z')
+        {
+            break;
+        }
+
+        character++;
+
+        if (character > 'z')
+        {
+            character = 'A';
+        }
+    }
+}
+
+// code{}school's solution:
+void loopAlphabetCodeSchool()
+{
+    char letter_increment = 'a';
+    while (letter_increment != '[')
+    {
+        char letter = letter_increment; // be able to put breakpoint here to see what has happened.
+        displayCharacter(letter);
+
+        if (letter_increment == 'z')
+        {
+            letter_increment = 'A';
+        }
+        else
+        {
+            letter_increment++;
+        }
+    }
+}
+
 int main()
 {
     int a = 0;
@@ -21,5 +101,16 @@ int main()
     {
         a += 10;
     }
+
+    loopLowercaseAlphabet();
+    std::cout << "Next assignment\n" << std::endl;
+    loopLowercaseThenUppercaseAlphabetWeird();
+
+    std::cout << "\nAnother solution:" << std::endl;
+    loopAlphabet();
+
+    std::cout << "\ncode school solution:" << std::endl;
+    loopAlphabetCodeSchool();
+
     return 0;
 }
