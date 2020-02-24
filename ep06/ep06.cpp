b3f3514bce7f302b58a0e0f42268473996aea268
teddy <teddyjh@protonmail.com>
2020-02-24 20:55:13 +0100

    add take_damage function, questions and answers
    We add the take_damage function which is found in the video and will be
    focused on when we are talking (or listening) about memory.

--- ep06/ep06.cpp
+++ ep06/ep06.cpp
@@ -1,6 +1,27 @@
+#include <iostream>
 
+int take_damage(int health, int damage);
 
 int main() 
 {
+
+    std::cout << take_damage(100, 11) << " health left after taking damage.\n";
+
     return 0;
+}
+
+int take_damage(int health, int damage)
+{
+    int new_health = health;
+
+    new_health = new_health - damage; // could have written: `new_health -= damage`
+
+    return new_health;
+
+ 
+    // How many variables are there in this function? A: 4 (return is a variable too)
+    // How big is the function? A: 4 bytes times 4 variables (int - 4 bytes) = 16 bytes (i.e. how much memory does it take)
+    // What is the variable that is returned? A: It is `return new_health`, it does not have a name but it is still a variable (int)
+    // How big is the return variable? A: 4 bytes, since it is an int
+
 }
