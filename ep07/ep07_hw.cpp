6c638e37ec51ed2eb998dfc28919df260c4576e5
teddy <teddyjh@protonmail.com>
2020-02-26 15:02:41 +0100

    add homework for this episode with its assignemnts

---
+++ ep07/ep07_hw.cpp
@@ -0,0 +1,17 @@
+
+// 01 assignment:
+// a) Practice creating pointers of the fundamental types 
+// b) Create a function that creates dynamic memory and returns it
+//      for example: a function that returns an int*
+
+// 02 assignment:
+// Create two classes:
+//  Class 01:
+//  - called `player`
+//  - has both `health` and `damage` as dynamic memory (pointers)
+//      cannot leak memory so remember to add destructor.
+//  Class 02:
+//  - called `combat`
+//  - takes into the constructor two player* pointers (as arguments)
+//  - Makes those two players fight until one is dead (loop through!)
+//      how else it is done, and how it return values is up to us.
