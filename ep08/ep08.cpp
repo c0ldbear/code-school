e161c5e4755ccfeb856ddc01116581964768a9b4
teddy <teddyjh@protonmail.com>
2020-02-28 10:02:17 +0100

    add more code to string example

--- ep08/ep08.cpp
+++ ep08/ep08.cpp
@@ -19,4 +19,13 @@ void stringExample()
 
     std::string aLongOne = name + " is the best in the whole world because the person just is that. The best. In. The. World.";
     std::cout << aLongOne << "\n";
+
+    if (name.empty())
+    {
+        return;
+    }
+
+    std::string shortName = name.substr(0, 3);
+    std::cout << shortName << "\n";
+
 }
