00ac3c23d9ec762bcaa0c79b4db17cd73f095d28
teddy <teddyjh@protonmail.com>
2020-02-28 13:31:16 +0100

    add struct player with damage and crit

--- ep09/ep09.cpp
+++ ep09/ep09.cpp
@@ -1,5 +1,49 @@
+#include <iostream>
+
+#include "ngon_random.h"
+
+struct player
+{
+    int health;
+
+    int damage_min;
+    int damage_max;
+
+    player(int aHealth, int aDamageMin, int aDamageMax);
+
+    int damage();
+    bool is_critical_hit();
+
+};
+
+player::player(int aHealth, int aDamageMin, int aDamageMax)
+{
+    health = aHealth;
+
+    damage_min = aDamageMin;
+    damage_max = aDamageMax;
+}
+
+int player::damage()
+{
+    return random_number(damage_min, damage_max);
+}
+
+bool player::is_critical_hit()
+{
+    int crit_result = random_number(1, 100);
+    
+    return crit_result > 68;
+}
 
 int main()
 {
+    player poly(100, 1, 12);
+    
+    std::cout << poly.damage() << "\n";
+    std::cout << poly.damage() << "\n";
+    std::cout << poly.damage() << "\n";
+    std::cout << poly.damage() << "\n";
+
     return 0;
 }
