32a6337428d70e136ea6b452a8f6092e6f38a186
teddy <teddyjh@protonmail.com>
2020-02-28 10:00:18 +0100

    add string example (`stringExample`)

--- ep08/ep08.cpp
+++ ep08/ep08.cpp
@@ -2,7 +2,21 @@
 #include <memory>
 #include <iostream> 
 
+void stringExample();
+
 int main() 
 {
+    stringExample();
+
     return 0;
+}
+
+void stringExample()
+// Trying out example from the video before code-along part
+{
+    std::string name = "Teddy";
+    std::cout << name << "\n";
+
+    std::string aLongOne = name + " is the best in the whole world because the person just is that. The best. In. The. World.";
+    std::cout << aLongOne << "\n";
 }
