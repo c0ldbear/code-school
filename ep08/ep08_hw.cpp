3b0127bc71264da9002ffd94f7a433b3f92f0401
teddy <teddyjh@protonmail.com>
2020-03-01 15:45:45 +0100

    add function templates for string, vector & shared_ptr

--- ep08/ep08_hw.cpp
+++ ep08/ep08_hw.cpp
@@ -4,11 +4,35 @@
 //  a) use the .empty(), .size(), .clear(), and .replace() functions
 //  b) How would you combine 2 strings into 1? (A: just add together with +)
 //      "Olafur" "Gabe" into "OlafurGabe" for example
+void stringPractice();
 // 2. Practice using std::shared_ptr
 //  a) Create a std::shared_ptr for all fundamental types
 //  b) Make one using std::string
 //  c) Send a std::shared_ptr to a function and also make a function return a std::shared_ptr 
+void shared_ptrPractice();
 // 3. Practice using std::vector
 //  a) Create a std::vector for all fundamental types
 //  b) Use the .empty(), .size(), .clear(), [] operator, and .front() function
 //  c) Try creating a std::vector of std::shared_ptr
+void vectorPractice();
+
+int main()
+{
+
+    return 0;
+}
+
+void stringPractice()
+{
+
+}
+
+void shared_ptrPractice()
+{
+
+}
+
+void vectorPractice()
+{
+
+}
